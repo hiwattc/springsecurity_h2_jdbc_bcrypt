3140dfaa306bbd9cf34daa473bf82cb731aba143
hiwattc@gmail.com <hiwattc@gmail.com>
2020-08-09 19:02:45 +0900

    2020.08.09

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @SpringBootApplication
 @RestController
@@ -18,4 +22,14 @@ public class DemoApplication {
 		return "hello wolrd";
 	}
 
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    @Autowired
+    UserRepository repository;
+    @GetMapping("/user")
+    public void user(){
+     repository.save(new User("hiwatt1"));
+        for(User user : repository.findAll()){
+           log.info(user.toString());
+        }
+    }
 }
